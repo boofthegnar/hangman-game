d2c3c744e2df4a94ffd95dbb488236105b187f1d
Dan Hogan <hogan.daniel@hotmail.com>
2019-04-28 14:18:59 -0700

    Removed unnecessary variables

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 import getWord from './getWord.js';
 import checkGuess from './checkGuess.js';
 
+// Stores all needed DOM elements into JS variables.
 const wordDisplay = document.getElementById('word-display');
-const underscoreDisplay = document.getElementById('underscore-display');
-const hangmanHead = document.getElementsByClassName('hangman head');
-const hangmanTorso = document.getElementsByClassName('hangman torso');
-const hangmanArm1 = document.getElementsByClassName('hangman arm1');
-const hangmanArm2 = document.getElementsByClassName('hangman arm2');
-const hangmanLeg1 = document.getElementsByClassName('hangman leg1');
-const hangmanLeg2 = document.getElementsByClassName('hangman leg2');
-const guessLetter = document.getElementsByClassName('guess-letter');
-const userInput = document.getElementsByClassName('user-input');
+const hangman = document.querySelectorAll('.hangman');
+const userInput = document.getElementById('userInput').value;
 const submitGuess = document.getElementById('submit-guess');
 
-let mockArray = Array(randomWord.length);
+for(let i = 1; i < hangman.length; i++) {
+    hangman[i].classList.add('hidden');
+}
+
+// Stores a random word within a variable based off Math.random and a list of words.
 let randomWord = getWord();
-let guess = userInput.value;
+
+// Creates an empty array with length equal to randomWord.
+let mockArray = Array(randomWord.length);
+console.log(mockArray);
 
 function createUnderscore(wordDisplay, randomWord) {
     for(let i = 0; i < randomWord.length; i++) {
@@ -28,4 +29,14 @@ function createUnderscore(wordDisplay, randomWord) {
 createUnderscore(wordDisplay, randomWord);
 
 submitGuess.addEventListener('click', () => {
+    // Checkes users guess and returns indices of correct guessed letters
+    const indices = checkGuess(userInput, randomWord);
+    if(indices.length) {
+        //Addes user guess to mock array based off indices
+        for(let i = 0; i < indices.length; i++) {
+            mockArray[indices[i]] = userInput;
+            wordDisplay.children[indices[i]].classList.remove('blank');
+            wordDisplay.children[indices[i]].textContent = userInput;
+        }
+    }
 });
