8d11c147637cab8b3ce13d4881e59306e492771c
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-01 09:23:43 -0700

    Created passing test for getWord function

--- src/getWord.js
+++ src/getWord.js
@@ -1,4 +1,4 @@
-let wordList = ['alligator', 'dog', 'steeze'];
+let wordList = ['boof', 'gnar', 'banana', 'lance', 'cucumber'];
 
 function getWord() {
     const randomNumber = Math.floor(Math.random() * wordList.length);
--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,17 @@
 import getWord from '../src/getWord.js';
-import checkGuess from '../src/checkGuess';
+import checkGuess from '../src/checkGuess.js';
 
 const test = QUnit.test;
 
+let wordList = ['boof', 'gnar', 'banana', 'lance', 'cucumber'];
+
 test('return a random word from a list of words', assert => {
     //arrange
-    const expected = 'boof';
-    //act 
     const result = getWord();
+    const expected = wordList.includes(result);
+
     //assert
-    assert.equal(expected, result);
+    assert.equal(expected, true);
 });
 
 test('returns the indices at which user guess is located in word', assert => {
